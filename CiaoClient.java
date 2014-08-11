2df27c915eb3726b5dbc32d42b1255c32f215206
Janitha Tennakoon <janitha000@gmail.com>
2014-08-11 22:59:42 +0530

    Common Chat

--- CiaoClient.java
+++ CiaoClient.java
@@ -1,71 +1,70 @@
+
 import CiaoApp.*;          // The package containing our stubs
 import org.omg.CosNaming.*; // HelloClient will use the naming service.
 import org.omg.CosNaming.NamingContextPackage.*;
 import org.omg.CORBA.*;     // All CORBA applications need these classes.
-import org.omg.PortableServer.*;   
+import org.omg.PortableServer.*;
 import org.omg.PortableServer.POA;
 import java.util.Scanner;
 
+class CiaoCallbackServant extends CiaoCallbackPOA {
 
- 
-class CiaoCallbackServant extends CiaoCallbackPOA
-{
     private ORB orb;
 
     public void setORB(ORB orb_val) {
         orb = orb_val;
     }
 
-    public void callback(String notification)
-    {
+    public void callback(String notification) {
         System.out.println(notification);
     }
 }
 
-public class CiaoClient
-{
+public class CiaoClient {
+
     static Ciao ciaoImpl;
-    
-    public static void main(String args[])
-    {
-	try {
-	    ORB orb = ORB.init(args, null);
-	    POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+
+    public static void main(String args[]) {
+        try {
+            ORB orb = ORB.init(args, null);
+            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
             rootpoa.the_POAManager().activate();
 
-	    org.omg.CORBA.Object objRef = 
-                     orb.resolve_initial_references("NameService");
-	    NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            org.omg.CORBA.Object objRef =
+                    orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
 
-	    String name = "Ciao";
-	    ciaoImpl = CiaoHelper.narrow(ncRef.resolve_str(name));
-	    
-	    CiaoCallbackServant ciaoCallbackImpl = new CiaoCallbackServant();
-	    ciaoCallbackImpl.setORB(orb);
+            String name = "Ciao";
+            ciaoImpl = CiaoHelper.narrow(ncRef.resolve_str(name));
 
-	    org.omg.CORBA.Object ref = rootpoa.servant_to_reference(ciaoCallbackImpl);
-	    CiaoCallback cref = CiaoCallbackHelper.narrow(ref);
-		
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter your UserName: ");
-		String id = in.nextLine();
-        boolean exit=false;
-		while(!exit){
-		System.out.print(id+ ": ");
-		String msg = in.nextLine();
-		if(msg.equals("exit")){
-		 exit=true;
-		 break;
-		 }
+            CiaoCallbackServant ciaoCallbackImpl = new CiaoCallbackServant();
+            ciaoCallbackImpl.setORB(orb);
 
-	    String ciao = ciaoImpl.say(cref,id,msg);
-	    //System.out.println(ciao);
-		
-		}
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(ciaoCallbackImpl);
+            CiaoCallback cref = CiaoCallbackHelper.narrow(ref);
+			
+			ciaoImpl.register(cref);
 
-	} catch(Exception e){
-	    System.out.println("ERROR : " + e);
-	    e.printStackTrace(System.out);
-	}
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter your UserName: ");
+            String id = in.nextLine();
+            boolean exit = false;
+            while (!exit) {
+                System.out.print("");
+                String msg = in.nextLine();
+                if (msg.equals("exit")) {
+                    exit = true;
+                    break;
+                }
+
+                String ciao = ciaoImpl.say(id, msg);
+                //System.out.println(ciao);
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("ERROR : " + e);
+            e.printStackTrace(System.out);
+        }
     }
 }
--- CiaoServer.java
+++ CiaoServer.java
@@ -1,57 +1,75 @@
+
 import CiaoApp.*;          // The package containing our stubs. 
+import java.util.ArrayList;
+import java.util.HashMap;
 import org.omg.CosNaming.*; // HelloServer will use the naming service. 
 import org.omg.CosNaming.NamingContextPackage.*; // ..for exceptions. 
 import org.omg.CORBA.*;     // All CORBA applications need these classes. 
-import org.omg.PortableServer.*;   
+import org.omg.PortableServer.*;
 import org.omg.PortableServer.POA;
- 
-class CiaoServant extends CiaoPOA
-{
+
+class CiaoServant extends CiaoPOA {
+
     private ORB orb;
+    private ArrayList<String> chatList;
+    public ArrayList<CiaoCallback> usersList;
 
     public void setORB(ORB orb_val) {
         orb = orb_val;
+		usersList= new ArrayList<>();
     }
+	
+	public void register(CiaoCallback callobj){
+		usersList.add(callobj);
+	}
 
-    public String say(CiaoCallback callobj, String id,String msg)
-    {
-        callobj.callback(id+": "+msg);
-		String msgToPrint= id+": "+msg;
-		return msgToPrint;
+    public String say( String id, String msg) {
+	String msgToPrint;
+		System.out.println(usersList.size());
+        //if (chatList == null) {
+        //    chatList = new ArrayList<>();
+        //}
+		//for(CiaoCallback obj : usersList){
+		for (int i = 0; i < usersList.size(); i++){
+		CiaoCallback obj= usersList.get(i);
+				System.out.println(id);
+
+        obj.callback(id + ": " + msg);
+         msgToPrint = id + ": " + msg;
+		}
+		return "YEEEPPEEEE";
         //return "\n Ciao Mondo!! \n";
     }
 }
 
-public class CiaoServer 
-{
-    public static void main(String args[]) 
-    {
-	try { 
-	    ORB orb = ORB.init(args, null); 
+public class CiaoServer {
 
-	    POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));  
-	    rootpoa.the_POAManager().activate(); 
+    public static void main(String args[]) {
+        try {
+            ORB orb = ORB.init(args, null);
 
-	    CiaoServant ciaoImpl = new CiaoServant();
-	    ciaoImpl.setORB(orb);  
+            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            rootpoa.the_POAManager().activate();
 
-	    org.omg.CORBA.Object ref = rootpoa.servant_to_reference(ciaoImpl);
-	    Ciao cref = CiaoHelper.narrow(ref);
+            CiaoServant ciaoImpl = new CiaoServant();
+            ciaoImpl.setORB(orb);
 
-	    org.omg.CORBA.Object objRef = 
-		           orb.resolve_initial_references("NameService");
-	    NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(ciaoImpl);
+            Ciao cref = CiaoHelper.narrow(ref);
 
-	    String name = "Ciao";
-	    NameComponent path[] = ncRef.to_name(name);
-	    ncRef.rebind(path, cref);
-	    
-	    orb.run();
-	}
-	    
-	catch(Exception e) {
-	    System.err.println("ERROR : " + e);
-	    e.printStackTrace(System.out);
-	}
+            org.omg.CORBA.Object objRef =
+                    orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+            String name = "Ciao";
+            NameComponent path[] = ncRef.to_name(name);
+            ncRef.rebind(path, cref);
+
+		    
+            orb.run();
+        } catch (Exception e) {
+            System.err.println("ERROR : " + e);
+            e.printStackTrace(System.out);
+        }
     }
 }
